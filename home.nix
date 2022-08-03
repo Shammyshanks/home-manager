2563af08ecb57682a4012f238cd9944bce39195f
Shammyshanks <thorinbriggz@gmail.com>
2022-08-03 14:32:34 -0700

    Add files via upload

---
+++ home.nix
@@ -0,0 +1,107 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "thor";
+  home.homeDirectory = "/home/thor";
+
+     
+
+  nixpkgs.config.packageOverrides = pkgs: {
+        nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+          inherit pkgs;
+        };
+      };
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.05";
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowBroken = true;
+
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableSyntaxHighlighting = true;
+    initExtra = ''
+      PS1='%B%{$fg[red]%}[%{$fg[yellow]%}%n%{$fg[green]%}@%{$fg[blue]%}%M %{$fg[magenta]%}%~%{$fg[red]%}]%{$reset_color%}$%b'
+'';
+    autocd = true;
+    defaultKeymap = "emacs";
+    dirHashes = {
+      docs = "$HOME/Documents";
+      dl = "$HOME/Downloads";
+      conf = "$HOME/.configs";
+    };
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" "sudo"];
+      theme = "agnoster";
+    };
+    shellGlobalAliases = {
+ls = "ls -a --color";
+neofetch = "neofetch --ascii_distro nixos_old";
+gitconfigs = "git clone https://github.com/shammyshanks/configs";
+gitemacs = "git clone https://github.com/shammyshanks/emacs-current";
+gitstump = "git clone https://github.com/shammyshanks/stumpwm";
+githerb = "git clone https://github.com/shammyshanks/herbstluftwm";
+gitpoly = "git clone https://github.com/shammyshanks/polybar";
+gitgruv = "git clone https://github.com/shammyshanks/gruvbox";
+gitkeys = "git clone https://github.com/shammyshanks/sxhkd";
+gitfont = "git clone https://github.com/shammyshanks/font";
+hc = "herbstclient";
+    };
+  };
+
+programs.qutebrowser = {
+  enable = true;
+  keyBindings = {
+    normal = {
+      "<CTRL-p>" = "scroll up";
+    };
+  };
+};
+  
+  
+
+programs.firefox = {
+  enable = true;
+  extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+    ublock-origin
+    darkreader
+    bypass-paywalls-clean
+    vim-vixen
+    vimium
+    stylus
+    bitwarden
+    decentraleyes
+    i-dont-care-about-cookies
+    https-everywhere
+    netflix-1080p
+    tree-style-tab
+    violentmonkey
+  ];
+  profiles.default = {
+    id = 0;
+    name = "Default";
+    isDefault = true;
+    settings = {
+  "browser.startup.homepage" = "https://nixos.org";
+
+    };
+  };
+};
+
+
+# Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
+
+
